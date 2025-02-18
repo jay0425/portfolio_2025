623e154927a0760b95c1ef3bb1eac797a694597b
jay0425 <heedev94@gmail.com>
2025-02-18 21:28:12 +0900

    App.jsx에 swiper 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,79 @@ import './App.css';
 
 import Header from './components/Header.presenter.jsx';
 
+// --------------------------------
+
+import { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Mousewheel, Keyboard, A11y } from 'swiper/modules';
+
+import styled from 'styled-components';
+
+import './App.css';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/keyboard';
+import 'swiper/css/mousewheel';
+
+import Intro from './pages/Intro.presenter.jsx';
+import Skill from './pages/Skill.presenter.jsx';
+import Portfolio from './pages/Portfolio.presenter.jsx';
+import AboutMe from './pages/AboutMe.presenter.jsx';
+
 function App() {
+  const [pageNum, setPageNum] = useState(0);
+  const swiperRef = useRef(null);
+
   return (
     <>
-      <Header />
+      <div className="App">
+        <Header swiperRef={swiperRef} pageNum={pageNum} />
+        <StyledSwiper
+          ref={swiperRef}
+          direction={'vertical'}
+          slidesPerView={1}
+          modules={[Mousewheel, Pagination, Keyboard, A11y]}
+          mousewheel={true}
+          keyboard={{ enabled: true }}
+          speed={600}
+          pagination={{ clickable: true }}
+          onSlideChange={(e) => {
+            setPageNum(e.activeIndex);
+          }}
+        >
+          <StyledSwiperSlide>
+            <Intro />
+          </StyledSwiperSlide>
+          <StyledSwiperSlide>
+            <Skill />
+          </StyledSwiperSlide>
+          <StyledSwiperSlide>
+            <Portfolio />
+          </StyledSwiperSlide>
+          <StyledSwiperSlide>
+            <AboutMe />
+          </StyledSwiperSlide>
+        </StyledSwiper>
+      </div>
     </>
   );
 }
 
 export default App;
+
+const StyledSwiper = styled(Swiper)`
+  width: 100%;
+  height: 100vh;
+  height: 100dvh;
+
+  .swiper-pagination {
+    .swiper-pagination-bullet {
+      background: var(--swiper-pagination-bullet-inactive-color, var(--sub-blue));
+    }
+    .swiper-pagination-bullet-active {
+      background: var(--swiper-pagination-bullet-inactive-color, var(--main-blue));
+      opacity: 0.6;
+    }
+  }
+`;
+const StyledSwiperSlide = styled(SwiperSlide)``;
