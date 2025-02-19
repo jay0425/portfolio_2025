47f0a23a627e55c3db5d3537763d276bd5315692
jay0425 <heedev94@gmail.com>
2025-02-19 19:26:41 +0900

    Skill html 구현

--- src/pages/Skill.presenter.jsx
+++ src/pages/Skill.presenter.jsx
@@ -1,7 +1,65 @@
+import * as S from './Skill.styles.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHtml5,
+  faCss3Alt,
+  faSass,
+  faJs,
+  faReact,
+  faSquareGit,
+  faSquareGithub,
+} from '@fortawesome/free-brands-svg-icons';
+import { faFileCode } from '@fortawesome/free-regular-svg-icons';
+import { faDisplay } from '@fortawesome/free-solid-svg-icons';
+
 const Skill = () => {
   return (
     <>
-      <h1>Skill</h1>
+      <section id="skill">
+        <h2>SKILL</h2>
+        <S.SkillUl>
+          <li>
+            <FontAwesomeIcon icon={faHtml5} size="3x" color="red" />
+            <p>html5</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faCss3Alt} size="3x" color="blue" />
+            <p>css3</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faSass} size="3x" color="rgb(121, 3, 83)" />
+            <p>scss</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faFileCode} size="3x" color="rgb(7, 158, 188)" />
+            <p>tailwind css</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faJs} size="3x" color="rgb(242, 245, 49)" />
+            <p>javascript</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faReact} size="3x" color="rgb(0, 209, 246)" />
+            <p>react</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faFileCode} size="3x" />
+            <p>react query</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faSquareGit} size="3x" color="rgb(244, 79, 79)" />
+            <p>git</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faSquareGithub} size="3x" />
+            <p>github</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faDisplay} size="3x" color="rgb(97, 239, 232)" />
+            <p>netlify</p>
+          </li>
+        </S.SkillUl>
+      </section>
     </>
   );
 };
---
+++ src/pages/Skill.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const SkillUl = styled.ul`
+  padding: 20px;
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  li {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
